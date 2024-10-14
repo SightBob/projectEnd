d17bad11c84b0323133262a4019e612f6e6953c4
S_ZOOM <pawatsaekoo@gmail.com>
2024-10-14 15:07:31 +0700

    แก้ ui

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -283,7 +283,7 @@ const Navbar = ({ toggleChat }) => {
 
               <div className="flex xl:hidden" onClick={ClickOpenMenu}>
               <div className={`text-black ${openMenu ? "open" : ""}`}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2" class="size-8" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeWidth="2" className="size-8" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/></svg>
               </div>
             </div>
             </>
@@ -344,7 +344,7 @@ const Navbar = ({ toggleChat }) => {
               </Link>
             <div className="flex xl:hidden" onClick={ClickOpenMenu}>
               <div className={`text-black ${openMenu ? "open" : ""}`}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2" class="size-8" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeWidth="2" className="size-8" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/></svg>
               </div>
             </div>
             </div>
